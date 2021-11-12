3348be2440bf6b3229f3b04ed0fc31e0900922ba
Satruijt <94087869+Satruijt@users.noreply.github.com>
2021-11-12 02:47:38 -0500

    Delete Lesson 7 directory

--- Lesson 7/Unit 5 | Lesson 7: Loops Practice | Loops and Screen Elements | 7.js
+++
@@ -1,6 +0,0 @@
-onEvent("randomizeButton", "click", function( ) {
-  for (var i = 0; i < 4; i++) {
-    setProperty("label"+i, "text-color", rgb(randomNumber(0,255),randomNumber(0,255),randomNumber(0,255)));
-    setProperty("label"+i, "font-size", randomNumber(2, 50));
-  }
-});
